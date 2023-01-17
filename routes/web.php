84c5c28ce9829f4918e00c715722ae85ce9ee43f
Furkan Çeker <ceker0901@gmail.com>
2023-01-17 20:31:59 +0300

    view & route operations

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,17 @@ Route::get('ornek/{isim}',function($isim){
 
     return view('deneme',['isim'=>$isim]);
 });
+
+Route::get('web',function(){
+    $data["title"] = "Laravel Eğitim";
+    $data["nav1"] = "Hakkımızda";
+    $data["nav2"] = "Hizmetler";
+    $data["nav3"] = "Galeri";
+    $data["nav4"] = "İletişim";
+    $data["masthead"] = "Laravel Eğitimleri";
+    $data["mastinfo"] = "Laravel Eğitimine Hoşgeldiniz!";
+    $data["abouttitle"] = "Sana İhtiyacımız Var";
+    $data["aboutinfo"] = "+10 dan Fazla Laravel Eğitimini Sizlere Sunuyoruz";
+
+    return view('web',$data);
+});
